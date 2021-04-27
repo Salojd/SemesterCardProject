ea176d399cfab00d72131ef0a6700717408d7692
bsilv22 <bsilv22@yahoo.com>
2021-04-27 17:52:04 -0400

    Adding System.out.print("TESTING")

--- src/playingcards24/PlayingCards24.java
+++ src/playingcards24/PlayingCards24.java
@@ -33,6 +33,8 @@ public class PlayingCards24 extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        
+        System.out.print("TESTING");
     }
     
 }
